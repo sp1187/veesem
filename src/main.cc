cdf42f7fb291df724971ec593f0a34b9f7488c48
Simon Eriksson <simon.eriksson.1187@gmail.com>
2026-01-10 23:57:07 +0100

    UI: Add support for selecting and loading V.Smile ROMs from UI

--- src/main.cc
+++ src/main.cc
@@ -12,6 +12,9 @@
 
 void PrintUsage(std::string exec_name) {
   std::cout
+      << "veesem (V.Smile emulator) version " VERSION << std::endl
+      << "Website: http://github.com/sp1187/veesem" << std::endl
+      << std::endl
       << "Usage: " << exec_name << " [OPTIONS] CARTROM" << std::endl
       << std::endl
       << "Options:" << std::endl
@@ -22,7 +25,8 @@ void PrintUsage(std::string exec_name) {
       << std::endl
       << "  -art-nvram FILE   Emulate CSB2 cartridge NVRAM and use FILE for persistent saving"
       << std::endl
-      << "  -region NUM       Set jumpers configuring system ROM region as hex number in range 0-f"
+      << "  -region NUM       Set jumpers configuring system ROM region as hex number in range "
+         "0-f"
       << std::endl
       << "  -novtech          Set jumpers disabling VTech logo in system ROM intro" << std::endl
       << std::endl
@@ -33,24 +37,17 @@ void PrintUsage(std::string exec_name) {
 }
 
 int main(int argc, char** argv) {
-  if (argc == 1) {
-    std::cout << "veesem (V.Smile emulator) version " VERSION << std::endl
-              << "Website: http://github.com/sp1187/veesem" << std::endl
-              << std::endl;
+  SystemConfig system_config;
+  system_config.cart_type = VSmile::CartType::STANDARD;
+  system_config.region_code = 0xe;  // UK English as default
+  system_config.vtech_logo = true;
+  system_config.video_timing = VideoTiming::PAL;
 
-    PrintUsage(argv[0]);
-    return EXIT_SUCCESS;
-  }
+  UiConfig ui_config;
+  ui_config.show_leds = false;
+  ui_config.show_fps = false;
+
   bool read_flags = true;
-  VSmile::CartType cart_type = VSmile::CartType::STANDARD;
-  bool vtech_logo = true;
-  bool show_leds = false;
-  bool show_fps = false;
-  VideoTiming video_timing = VideoTiming::PAL;
-  std::optional<std::string> sysrom_path;
-  std::optional<std::string> cartrom_path;
-  std::optional<std::string> art_nvram_path;
-  unsigned region_code = 0xe;  // UK English as default
   const std::vector<std::string_view> args(argv + 1, argv + argc);
 
   size_t argpos = 0;
@@ -63,23 +60,23 @@ int main(int argc, char** argv) {
       }
       if (arg == "-sysrom") {
         if (argpos + 1 >= args.size()) {
-          std::cerr << "Error: Expected system ROM path" << std::endl;
+          std::cerr << "Argument error: Expected system ROM path" << std::endl;
           return EXIT_FAILURE;
         }
-        sysrom_path = args[++argpos];
+        system_config.sysrom_path = args[++argpos];
       } else if (arg == "-art-nvram") {
-        cart_type = VSmile::CartType::ART_STUDIO;
+        system_config.cart_type = VSmile::CartType::ART_STUDIO;
         if (argpos + 1 >= args.size()) {
-          std::cerr << "Error: Expected Art Studio NVRAM path" << std::endl;
+          std::cerr << "Argument error: Expected Art Studio NVRAM path" << std::endl;
           return EXIT_FAILURE;
         }
-        art_nvram_path = args[++argpos];
+        system_config.csb2_nvram_save_path = args[++argpos];
       } else if (arg == "-ntsc") {
-        video_timing = VideoTiming::NTSC;
+        system_config.video_timing = VideoTiming::NTSC;
       } else if (arg == "-pal") {
-        video_timing = VideoTiming::PAL;
+        system_config.video_timing = VideoTiming::PAL;
       } else if (arg == "-art") {
-        cart_type = VSmile::CartType::ART_STUDIO;
+        system_config.cart_type = VSmile::CartType::ART_STUDIO;
       } else if (arg == "-region") {
         if (argpos + 1 >= args.size()) {
           std::cerr << "Error: Expected system region code" << std::endl;
@@ -87,41 +84,42 @@ int main(int argc, char** argv) {
         }
         const auto& num_str = args[++argpos];
 
-        auto [ptr, error] =
-            std::from_chars(num_str.data(), num_str.data() + num_str.size(), region_code, 16);
+        auto [ptr, error] = std::from_chars(num_str.data(), num_str.data() + num_str.size(),
+                                            system_config.region_code, 16);
 
         if (ptr != (num_str.data() + num_str.size()) || error != std::errc()) {
-          std::cerr << "Error: could not parse region code as hex value" << std::endl;
+          std::cerr << "Argument error: could not parse region code as hex value" << std::endl;
           return EXIT_FAILURE;
         }
 
-        if (region_code > 0xf) {
-          std::cerr << "Error: Region code out of range (should be in range 0-f)" << std::endl;
+        if (system_config.region_code > 0xf) {
+          std::cerr << "Argument error: Region code out of range (should be in range 0-f)"
+                    << std::endl;
           return EXIT_FAILURE;
         }
       } else if (arg == "-novtech") {
-        vtech_logo = false;
+        system_config.vtech_logo = false;
       } else if (arg == "-leds") {
-        show_leds = true;
+        ui_config.show_leds = true;
       } else if (arg == "-fps") {
-        show_fps = true;
+        ui_config.show_fps = true;
       } else if (arg == "--") {
         read_flags = false;
       } else {
-        std::cerr << "Error: Unknown flag " << arg << std::endl;
+        std::cerr << "Argument error: Unknown flag " << arg << std::endl << std::endl;
+        PrintUsage(argv[0]);
         return EXIT_FAILURE;
       }
     } else {
-      if (!cartrom_path.has_value()) {
-        cartrom_path = arg;
+      if (!system_config.cartrom_path.has_value()) {
+        system_config.cartrom_path = arg;
       } else {
-        std::cerr << "Error: too many ROM arguments sent" << std::endl;
+        std::cerr << "Argument error: too many ROM arguments sent" << std::endl;
         return EXIT_FAILURE;
       }
     }
     argpos++;
   }
 
-  return RunEmulation(sysrom_path, cartrom_path, cart_type, art_nvram_path, region_code, vtech_logo,
-                      video_timing, show_leds, show_fps);
+  return RunEmulation(system_config, ui_config);
 }
--- src/ui/ui.cc
+++ src/ui/ui.cc
@@ -9,6 +9,10 @@
 
 #include "imgui_impl_opengl2.h"
 #include "imgui_impl_sdl2.h"
+#include "imgui_stdlib.h"
+
+#include "nfd.hpp"
+#include "nfd_sdl2.h"
 
 #include "core/vsmile/vsmile.h"
 #include "graphics_state.h"
@@ -26,6 +30,7 @@ static struct UiSettings {
   bool bilinear = true;
   bool show_ppu_view_settings_window = false;
   bool show_spu_output_window = false;
+  bool show_load_window = false;
 
   std::array<float, 281250 / 4> audio_samples_left;
   std::array<float, 281250 / 4> audio_samples_right;
@@ -33,6 +38,11 @@ static struct UiSettings {
 
   PpuViewSettings ppu_view_settings = {};
 } ui;
+
+static std::unique_ptr<VSmile> vsmile = nullptr;
+static GraphicsState graphics_state;
+
+static SystemConfig cur_system_config;
 
 static void SdlAudioCallback(void* audio_stream, unsigned char* output, int len) {
   int written = SDL_AudioStreamGet(static_cast<SDL_AudioStream*>(audio_stream), output, len);
@@ -92,7 +102,7 @@ static VSmile::JoyInput ReadControllerFromKeyboard() {
   return input;
 }
 
-static bool ImguiInit(GraphicsState& graphics_state) {
+static bool ImguiInit() {
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGui::StyleColorsDark();
@@ -121,6 +131,237 @@ static void DrawFps(bool show_number) {
   ImGui::PopStyleVar();
 }
 
+static void UnloadVSmile() {
+  if (!vsmile)
+    return;
+
+  // Flush Art Studio cartridge RAM to file if save path is defined
+  if (cur_system_config.cart_type == VSmile::CartType::ART_STUDIO &&
+      cur_system_config.csb2_nvram_save_path.has_value()) {
+    std::ofstream csb2_nvram_save(*cur_system_config.csb2_nvram_save_path,
+                                  std::ios::binary | std::ios::trunc);
+    if (!csb2_nvram_save.good()) {
+      std::cerr << "Failed to open NVRAM save file for writing" << std::endl;
+    } else {
+      auto final_csb2_nvram = *vsmile->GetArtNvram();
+      std::transform(final_csb2_nvram.begin(), final_csb2_nvram.end(), final_csb2_nvram.begin(),
+                     [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
+      csb2_nvram_save.write(reinterpret_cast<char*>(final_csb2_nvram.data()),
+                            sizeof(VSmile::ArtNvramType));
+      if (csb2_nvram_save.fail()) {
+        std::cerr << "Failed to write to NVRAM save file" << std::endl;
+      }
+    }
+  }
+
+  vsmile.reset();
+}
+
+static std::optional<std::string> LoadVSmile(const SystemConfig& config) {
+  if (!config.cartrom_path.has_value()) {
+    return "No cartridge ROM defined";
+  }
+
+  auto cartrom = std::make_unique<VSmile::CartRomType>();
+  std::ifstream cartrom_file(*config.cartrom_path, std::ios::binary);
+  if (!cartrom_file.good()) {
+    return "Could not open cartridge ROM file";
+  }
+  cartrom_file.read(reinterpret_cast<char*>(cartrom.get()), sizeof *cartrom);
+  std::transform(cartrom->begin(), cartrom->end(), cartrom->begin(),
+                 [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
+
+  auto sysrom = std::make_unique<VSmile::SysRomType>();
+  if (config.sysrom_path.has_value()) {
+    std::ifstream sysrom_file(*config.sysrom_path, std::ios::binary);
+    if (!sysrom_file.good()) {
+      return "Could not open system ROM file";
+    }
+    sysrom_file.read(reinterpret_cast<char*>(sysrom.get()), sizeof *sysrom);
+    std::transform(sysrom->begin(), sysrom->end(), sysrom->begin(),
+                   [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
+  } else {
+    // Provide game-compatible dummy system ROM if no one provided by user
+    sysrom->fill(0);
+    for (int i = 0xfffc0; i < 0xfffdc; i += 2) {
+      (*sysrom)[i + 1] = 0x31;
+    }
+  }
+
+  std::unique_ptr<VSmile::ArtNvramType> initial_art_nvram;
+  if (config.cart_type == VSmile::CartType::ART_STUDIO) {
+    if (config.csb2_nvram_save_path.has_value()) {
+      std::ifstream csb2_nvram_save_file(*config.csb2_nvram_save_path, std::ios::binary);
+      initial_art_nvram = std::make_unique<VSmile::ArtNvramType>();
+      if (csb2_nvram_save_file.good()) {
+        csb2_nvram_save_file.read(reinterpret_cast<char*>(initial_art_nvram.get()),
+                                  sizeof *initial_art_nvram);
+        std::transform(initial_art_nvram->begin(), initial_art_nvram->end(),
+                       initial_art_nvram->begin(),
+                       [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
+      }
+    } else {
+      initial_art_nvram = std::make_unique<VSmile::ArtNvramType>();
+    }
+  }
+
+  if (vsmile) {
+    UnloadVSmile();
+  }
+
+  vsmile = std::make_unique<VSmile>(std::move(sysrom), std::move(cartrom), config.cart_type,
+                                    std::move(initial_art_nvram), config.region_code,
+                                    config.vtech_logo, config.video_timing);
+  vsmile->Reset();
+  cur_system_config = config;
+
+  return {};
+}
+
+static void DrawLoadWindow() {
+  static std::string cartrom_path = cur_system_config.cartrom_path.value_or("");
+  static std::string csb2_nvram_save_path = cur_system_config.csb2_nvram_save_path.value_or("");
+  static std::string sysrom_path = cur_system_config.sysrom_path.value_or("");
+
+  static VideoTiming video_timing = cur_system_config.video_timing;
+  static bool enable_csb2_nvram = cur_system_config.cart_type == VSmile::CartType::ART_STUDIO;
+  static bool use_dummy_sysrom = sysrom_path.empty();
+  static int region_code = cur_system_config.region_code;
+  static bool vtech_logo = cur_system_config.vtech_logo;
+
+  static std::string load_error_string;
+
+  ImGui::SetNextWindowSize(ImVec2(480, 0), ImGuiCond_FirstUseEver);
+  ImGui::Begin("Load V.Smile ROM", &ui.show_load_window);
+
+  ImGui::SeparatorText("Cartridge");
+  ImGui::Text("Cartridge ROM path:");
+  ImGui::PushItemWidth(-50);
+  ImGui::InputText("##cartrom", &cartrom_path);
+  ImGui::PopItemWidth();
+  ImGui::SameLine();
+  if (ImGui::Button("...##cartrom_open")) {
+    nfdu8char_t* nfd_path;
+    nfdu8filteritem_t nfd_filters[] = {{"V.Smile ROM", "bin,vsmile"}};
+
+    nfdwindowhandle_t nfd_window_handle = {NFD_WINDOW_HANDLE_TYPE_UNSET, NULL};
+    NFD_GetNativeWindowFromSDLWindow(graphics_state.GetWindow(), &nfd_window_handle);
+
+    nfdresult_t result =
+        NFD::OpenDialog(nfd_path, nfd_filters, std::size(nfd_filters), nullptr, nfd_window_handle);
+    if (result == NFD_OKAY) {
+      cartrom_path = std::string(nfd_path);
+      NFD::FreePath(nfd_path);
+    }
+  }
+
+  ImGui::Dummy(ImVec2(0.0f, 15.0f));
+
+  ImGui::Checkbox("Enable CSB2 NVRAM (used by Art Studio)", &enable_csb2_nvram);
+  ImGui::BeginDisabled(!enable_csb2_nvram);
+  ImGui::Text("CSB2 NVRAM save:");
+  ImGui::PushItemWidth(-50);
+  ImGui::InputText("##csb2_nvram", &csb2_nvram_save_path);
+  ImGui::PopItemWidth();
+  ImGui::SameLine();
+  if (ImGui::Button("...##csb2_nvram_open")) {
+    nfdu8char_t* nfd_path;
+
+    nfdwindowhandle_t nfd_window_handle = {NFD_WINDOW_HANDLE_TYPE_UNSET, NULL};
+    NFD_GetNativeWindowFromSDLWindow(graphics_state.GetWindow(), &nfd_window_handle);
+
+    nfdresult_t result = NFD::SaveDialog(nfd_path, nullptr, 0, nullptr, nullptr, nfd_window_handle);
+    if (result == NFD_OKAY) {
+      csb2_nvram_save_path = std::string(nfd_path);
+      NFD::FreePath(nfd_path);
+    }
+  }
+  ImGui::EndDisabled();
+
+  ImGui::SeparatorText("System");
+  ImGui::RadioButton("PAL", reinterpret_cast<int*>(&video_timing),
+                     static_cast<int>(VideoTiming::PAL));
+  ImGui::SameLine();
+  ImGui::RadioButton("NTSC", reinterpret_cast<int*>(&video_timing),
+                     static_cast<int>(VideoTiming::NTSC));
+
+  ImGui::Dummy(ImVec2(0.0f, 15.0f));
+
+  ImGui::Checkbox("Use dummy system ROM", &use_dummy_sysrom);
+  ImGui::BeginDisabled(use_dummy_sysrom);
+  ImGui::Text("System ROM path:");
+  ImGui::PushItemWidth(-50);
+  ImGui::InputText("##sysrom", &sysrom_path);
+  ImGui::PopItemWidth();
+  ImGui::SameLine();
+  if (ImGui::Button("...##sysrom_open")) {
+    {
+      nfdu8char_t* nfd_path;
+
+      nfdwindowhandle_t nfd_window_handle = {NFD_WINDOW_HANDLE_TYPE_UNSET, NULL};
+      NFD_GetNativeWindowFromSDLWindow(graphics_state.GetWindow(), &nfd_window_handle);
+
+      nfdresult_t result = NFD::OpenDialog(nfd_path, nullptr, 0, nullptr, nfd_window_handle);
+      if (result == NFD_OKAY) {
+        sysrom_path = std::string(nfd_path);
+        NFD::FreePath(nfd_path);
+      }
+    }
+  }
+  ImGui::EndDisabled();
+
+  ImGui::Dummy(ImVec2(0.0f, 15.0f));
+
+  ImGui::Text("Jumpers:");
+  ImGui::PushItemWidth(100);
+  ImGui::SliderInt("Region code", &region_code, 0, 15, "0x%X");
+  ImGui::PopItemWidth();
+  ImGui::SameLine();
+  ImGui::Checkbox("Display VTech logo", &vtech_logo);
+
+  ImGui::Dummy(ImVec2(0.0f, 15.0f));
+  if (ImGui::Button("Load")) {
+    SystemConfig system_config;
+    if (!cartrom_path.empty())
+      system_config.cartrom_path = cartrom_path;
+    if (!sysrom_path.empty() && !use_dummy_sysrom)
+      system_config.sysrom_path = sysrom_path;
+    if (!csb2_nvram_save_path.empty())
+      system_config.csb2_nvram_save_path = csb2_nvram_save_path;
+
+    system_config.cart_type =
+        enable_csb2_nvram ? VSmile::CartType::ART_STUDIO : VSmile::CartType::STANDARD;
+    system_config.video_timing = video_timing;
+    system_config.region_code = region_code;
+    system_config.vtech_logo = vtech_logo;
+
+    auto maybe_error = LoadVSmile(system_config);
+    if (!maybe_error) {
+      ui.show_load_window = false;
+    } else {
+      load_error_string = *maybe_error;
+      ImGui::OpenPopup("Load Error");
+    }
+  }
+  ImGui::SameLine();
+  if (ImGui::Button("Close")) {
+    ui.show_load_window = false;
+  }
+
+  auto center = ImGui::GetMainViewport()->GetCenter();
+  ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+  if (ImGui::BeginPopupModal("Load Error", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
+    ImGui::Text(load_error_string.c_str());
+    ImGui::Dummy(ImVec2(0.0f, 5.0f));
+    if (ImGui::Button("OK")) {
+      ImGui::CloseCurrentPopup();
+    }
+    ImGui::EndPopup();
+  }
+
+  ImGui::End();
+}
+
 static void DrawLeds(VSmile::JoyLedStatus leds, ImVec2 overlay_pos) {
   ImGui::SetNextWindowPos(overlay_pos, ImGuiCond_Always, ImVec2(1.0, 1.0));
   ImGui::SetNextWindowBgAlpha(0.64f);
@@ -145,17 +386,22 @@ static void DrawLeds(VSmile::JoyLedStatus leds, ImVec2 overlay_pos) {
   ImGui::PopStyleVar();
 }
 
-static void DrawGui(GraphicsState& graphics_state, VSmile& vsmile) {
+static void DrawGui() {
   ImGuiIO& io = ImGui::GetIO();
   ui.frame_advance = false;
-  if (SDL_GetMouseFocus() && !ui.fullscreen && ImGui::BeginMainMenuBar()) {
+  if ((!vsmile || (SDL_GetMouseFocus() && !ui.fullscreen)) && ImGui::BeginMainMenuBar()) {
     if (ImGui::BeginMenu("Emulation")) {
+      if (ImGui::MenuItem("Load V.Smile ROM", "")) {
+        ui.show_load_window = true;
+      }
       ImGui::MenuItem("Run", "", &ui.run_emulation);
       ImGui::MenuItem("Unlock Framerate", "", &ui.unlock_framerate);
       ImGui::MenuItem("Frame Advance", "", &ui.frame_advance);
+      ImGui::BeginDisabled(!vsmile);
       if (ImGui::MenuItem("Hard Reset")) {
-        vsmile.Reset();
+        vsmile->Reset();
       }
+      ImGui::EndDisabled();
       ImGui::Separator();
       ImGui::MenuItem("ON Button", "F1", &ui.on_button);
       ImGui::MenuItem("OFF Button", "F2", &ui.off_button);
@@ -215,8 +461,8 @@ static void DrawGui(GraphicsState& graphics_state, VSmile& vsmile) {
     if (!ui.ppu_view_settings.show_sprites) {
       ImGui::EndDisabled();
     }
-    if (updated) {
-      vsmile.SetPpuViewSettings(ui.ppu_view_settings);
+    if (updated && vsmile) {
+      vsmile->SetPpuViewSettings(ui.ppu_view_settings);  // Do this on load???
     }
     ImGui::End();
   }
@@ -239,81 +485,42 @@ static void DrawGui(GraphicsState& graphics_state, VSmile& vsmile) {
     ImGui::End();
   }
 
+  if (ui.show_load_window) {
+    DrawLoadWindow();
+  }
+
   if (ui.show_leds) {
-    DrawLeds(vsmile.GetControllerLed(), ImVec2(io.DisplaySize.x - 4, io.DisplaySize.y - 4));
+    DrawLeds(vsmile ? vsmile->GetControllerLed() : VSmile::JoyLedStatus{},
+             ImVec2(io.DisplaySize.x - 4, io.DisplaySize.y - 4));
   }
   if (ui.show_fps) {
     DrawFps(ui.run_emulation);
   }
 }
 
-int RunEmulation(std::optional<std::string> sysrom_path, std::optional<std::string> cartrom_path,
-                 VSmile::CartType cart_type, std::optional<std::string> art_nvram_path,
-                 unsigned region_code, bool vtech_logo, VideoTiming video_timing, bool show_leds,
-                 bool show_fps) {
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
-    std::cerr << "Unable to initialize SDL";
-    return EXIT_FAILURE;
-  }
-
-  if (!cartrom_path.has_value()) {
-    std::cerr << "Error: No cartridge ROM defined" << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  auto sysrom = std::make_unique<VSmile::SysRomType>();
-  if (sysrom_path.has_value()) {
-    std::ifstream sysrom_file(sysrom_path.value(), std::ios::binary);
-    if (!sysrom_file.good()) {
-      std::cerr << "Error: Could not open system ROM file" << std::endl;
+int RunEmulation(const SystemConfig& system_config, const UiConfig& ui_config) {
+  if (system_config.cartrom_path.has_value()) {
+    auto load_error = LoadVSmile(system_config);
+    if (load_error.has_value()) {
+      std::cerr << "Load Error: " << *load_error << std::endl;
       return EXIT_FAILURE;
     }
-    sysrom_file.read(reinterpret_cast<char*>(sysrom.get()), sizeof *sysrom);
-    std::transform(sysrom->begin(), sysrom->end(), sysrom->begin(),
-                   [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
   } else {
-    // Provide game-compatible dummy system ROM if no one provided by user
-    sysrom->fill(0);
-    for (int i = 0xfffc0; i < 0xfffdc; i += 2) {
-      (*sysrom)[i + 1] = 0x31;
-    }
+    // Save system config so that values from command-line arguments
+    // are auto-filled in load window.
+
+    cur_system_config = system_config;
   }
 
-  auto cartrom = std::make_unique<VSmile::CartRomType>();
-  std::ifstream cartrom_file(cartrom_path.value(), std::ios::binary);
-  if (!cartrom_file.good()) {
-    std::cerr << "Error: Could not open cartridge ROM file" << std::endl;
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
+    std::cerr << "Platform Error: Unable to initialize SDL";
     return EXIT_FAILURE;
   }
-  cartrom_file.read(reinterpret_cast<char*>(cartrom.get()), sizeof *cartrom);
-  std::transform(cartrom->begin(), cartrom->end(), cartrom->begin(),
-                 [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
 
-  std::unique_ptr<VSmile::ArtNvramType> initial_art_nvram;
-  if (cart_type == VSmile::CartType::ART_STUDIO) {
-    if (art_nvram_path.has_value()) {
-      std::ifstream art_nvram_file(art_nvram_path.value(), std::ios::binary);
-      initial_art_nvram = std::make_unique<VSmile::ArtNvramType>();
-      if (art_nvram_file.good()) {
-        art_nvram_file.read(reinterpret_cast<char*>(initial_art_nvram.get()),
-                            sizeof *initial_art_nvram);
-        std::transform(initial_art_nvram->begin(), initial_art_nvram->end(),
-                       initial_art_nvram->begin(),
-                       [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
-      }
-    } else {
-      initial_art_nvram = std::make_unique<VSmile::ArtNvramType>();
-    }
-  }
-
-  auto vsmile =
-      std::make_unique<VSmile>(std::move(sysrom), std::move(cartrom), cart_type,
-                               std::move(initial_art_nvram), region_code, vtech_logo, video_timing);
-  vsmile->Reset();
-
-  GraphicsState graphics_state;
   graphics_state.Init(640, 480);
-  ImguiInit(graphics_state);
+  ImguiInit();
+
+  NFD::Init();
 
   SDL_GameController* pad = nullptr;
 
@@ -338,8 +545,8 @@ int RunEmulation(std::optional<std::string> sysrom_path, std::optional<std::stri
   SDL_Event e;
   bool quit = false;
 
-  ui.show_leds = show_leds;
-  ui.show_fps = show_fps;
+  ui.show_leds = ui_config.show_leds;
+  ui.show_fps = ui_config.show_fps;
 
   while (!quit) {
     while (SDL_PollEvent(&e) != 0) {
@@ -368,11 +575,11 @@ int RunEmulation(std::optional<std::string> sysrom_path, std::optional<std::stri
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
 
-    DrawGui(graphics_state, *vsmile);
+    DrawGui();
 
     bool fast_forward = ImGui::IsKeyDown(ImGuiKey_Tab) || ui.unlock_framerate;
 
-    if (ui.run_emulation || ui.frame_advance) {
+    if (vsmile && (ui.run_emulation || ui.frame_advance)) {
       if (pad) {
         vsmile->UpdateJoystick(ReadController(pad));
       } else {
@@ -403,8 +610,12 @@ int RunEmulation(std::optional<std::string> sysrom_path, std::optional<std::stri
       }
     }
 
-    auto fb = vsmile->GetPicture();
-    graphics_state.DrawFrame(fb.data(), ui.bilinear);
+    if (vsmile) {
+      auto fb = vsmile->GetPicture();
+      graphics_state.DrawFrame(fb.data(), ui.bilinear);
+    } else {
+      graphics_state.ClearFrame();
+    }
 
     ImGui::Render();
     ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
@@ -418,27 +629,12 @@ int RunEmulation(std::optional<std::string> sysrom_path, std::optional<std::stri
         SDL_Delay(10);  // better way to sync????
       }
     }
-    if (!ui.run_emulation) {
+    if (!vsmile || !ui.run_emulation) {
       SDL_Delay(20);
     }
   }
 
-  // Flush Art Studio cartridge RAM to file if save path is defined
-  if (cart_type == VSmile::CartType::ART_STUDIO && art_nvram_path.has_value()) {
-    std::ofstream art_nvram_save(art_nvram_path.value(), std::ios::binary | std::ios::trunc);
-    if (!art_nvram_save.good()) {
-      std::cerr << "Failed to open NVRAM save file for writing" << std::endl;
-    } else {
-      auto final_art_nvram = *vsmile->GetArtNvram();
-      std::transform(final_art_nvram.begin(), final_art_nvram.end(), final_art_nvram.begin(),
-                     [](uint16_t x) -> uint16_t { return SDL_SwapLE16(x); });
-      art_nvram_save.write(reinterpret_cast<char*>(final_art_nvram.data()),
-                           sizeof(VSmile::ArtNvramType));
-      if (art_nvram_save.fail()) {
-        std::cerr << "Failed to write to NVRAM save file" << std::endl;
-      }
-    }
-  }
+  UnloadVSmile();
 
   SDL_Quit();
   return EXIT_SUCCESS;
